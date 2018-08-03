3aeb965681315686e01bfe5d903c9818d38301cf
Lucas Bogerd <l.bogerd@protonmail.com>
2018-08-03 12:26:13 +0200

    added ef, created models, added first migration, ran first migration, added .db to gitignore

---
+++ wutdo.api/Models/WutdocContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wutdo.api.Models
+{
+    public class WutdocContext : DbContext
+    {
+        public DbSet<Poll> Polls { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=wutdo.db");
+        }
+    }
+}
